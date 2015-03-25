57a8822090dc24eb7491edbd142657e9b88449e4
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-25 16:27:36 -0400

    have ugly, but working code for seeding the teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+require 'pry'
+
+CONFERENCES = ['AFC', 'NFC']
+DIVISIONS = ['North', 'South', 'East', 'West']
+
+Conference.find_or_create_by(name: 'AFC')
+Conference.find_or_create_by(name: 'NFC')
+
+
+DIVISIONS.each do |division|
+  CONFERENCES.each do |conference|
+    Division.find_or_create_by(
+    name: "#{conference} #{division}",
+    conference_id: Conference.find_by(name: conference).id
+    )
+  end
+end
+
+
+teams = 'teams.csv'
+
+
+CSV.foreach(teams, headers: true) do |row|
+  team_name = row[0]
+  division = Division.find_by(name: row[1]).id
+  Team.find_or_create_by(name: team_name, division_id: division)
+end
