4067a064d0b3e8a7390f387c32364b7fe03b204b
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-30 13:55:47 -0400

    updated for changes to gem, pg vs sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150325162622) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "conferences", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at"
