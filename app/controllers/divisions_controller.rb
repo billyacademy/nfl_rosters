773f784ef88ad753c61a1238103a041d5c46db38
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-05 12:34:19 -0400

    added division id to json being rendered

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -1,6 +1,6 @@
 class DivisionsController < ApplicationController
   def index
-    @divisions =  Division.all.as_json(include: { :conference => { only: :name }})
+    @divisions =  Division.all.as_json(include: { :conference => { only: [:name, :id] }})
     render json: JSON.pretty_generate(@divisions)
   end
 end
