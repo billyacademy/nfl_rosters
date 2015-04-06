2394cf4b47c8702fcefca7b370f1db4a9d85a85c
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-06 18:22:55 -0400

    updated schema of db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325162622) do
+ActiveRecord::Schema.define(version: 20150406203910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20150325162622) do
   create_table "divisions", force: :cascade do |t|
     t.integer  "conference_id", null: false
     t.string   "name",          null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "position"
+    t.integer  "team_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
