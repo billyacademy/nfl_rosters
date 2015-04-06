fe3088586b0dae9a8829a21d760c4f00d9b4cdc5
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-06 18:39:26 -0400

    added route for players

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :conferences, only: [:index]
   resources :teams, only: [:index, :show]
   resources :divisions, only: [:index]
+  resources :players, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
