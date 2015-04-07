69b49ee5ffd3ef22e7d9b2016a9fff026b33724b
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-07 14:21:01 -0400

    added a class method to create conferences when seeding data

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,4 +1,10 @@
 class Conference < ActiveRecord::Base
   has_many :divisions
   has_many :teams, through: :divisions
+
+  def self.create_conferences(conferences)
+    conferences.each do |conference|
+      Conference.find_or_create_by(name: conference)
+    end
+  end
 end
