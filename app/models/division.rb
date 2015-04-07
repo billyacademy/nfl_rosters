c09c9bf9ec2d37fc26606c972516efafd40234fe
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-07 14:21:54 -0400

    added a class method to create divisions  based on regions and conferences.. nested do block here, may be opportunity to improve code

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,4 +1,15 @@
 class Division < ActiveRecord::Base
   belongs_to :conference
   has_many :teams
+
+  def self.create_divisions(regions, conferences)
+    regions.each do |region|
+       conferences.each do |conference|
+         Division.find_or_create_by(
+         name: "#{conference} #{region}",
+         conference_id: Conference.find_by(name: conference).id
+         )
+      end
+    end
+  end
 end
