e21045482856f675f2ea0b419f3a04c746209364
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-07 14:22:22 -0400

    added class method to create teams from csv  file

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,12 @@
 class Team < ActiveRecord::Base
   belongs_to :division
   has_many :players
+
+  def self.create_teams(team_csv)
+    CSV.foreach(team_csv, headers: true) do |row|
+      team_name = row[0]
+      division = Division.find_by(name: row[1]).id
+      Team.find_or_create_by(name: team_name, division_id: division)
+    end
+  end
 end
